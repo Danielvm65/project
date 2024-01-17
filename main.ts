dca1bf16aa7c85b350624a5e0f3d18ad833523df
Danielvm65 <156251834+Danielvm65@users.noreply.github.com>
2024-01-17 16:06:26 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -182,3 +182,6 @@ let CAMERA = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 scene.cameraFollowSprite(CAMERA)
+game.onUpdate(function () {
+    CAMERA.setPosition((p1.x + p2.x) / 2, (p1.y + p2.y) / 2)
+})
